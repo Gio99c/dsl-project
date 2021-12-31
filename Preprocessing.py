4eb92a5fa6aa7cd848eaac1028f47f7a35f3807c
Gio99c <50328709+Gio99c@users.noreply.github.com>
2021-12-31 12:01:35 +0100

    Preprocessing update

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,3 +1,4 @@
+from os import device_encoding
 import numpy as np
 import pandas as pd
 #import regex as re
@@ -7,6 +8,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from nltk.tokenize import word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords as sw
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from textblob import TextBlob
+from tqdm import tqdm
 
 class LemmaTokenizer(object):
     def __init__(self):
@@ -20,19 +24,30 @@ class LemmaTokenizer(object):
         return lemmas
 
 
-def load():
-    return pd.read_csv("development.csv")
+def load(filepath="./DSL2122_january_dataset/development.csv") -> pd.DataFrame:
+    try:
+        return pd.read_csv(filepath, encoding='utf-8')
+    except FileNotFoundError:
+        print('File not found')
 
-def cleaning(tweets):
-    tweets[["day_of_week", "month_of_year", "day_of_month", "time", "tz", "year"]] = tweets['date'].str.split(' ', expand=True)
-    tweets[["hour_of_day", "minute", "second"]] = tweets['time'].str.split(':', expand=True)
-    tweets.drop(columns=["date", "time"], inplace=True)
-    tweets.drop(columns=["tz", "year", "minute", "second", "flag"], inplace=True)
-    #tweets["text"] = tweets["text"][tweets.duplicated(subset=["text"], keep="first")] #duplicated removal - work in progress
+def cleaning(tweets: pd.DataFrame) -> pd.DataFrame:
+    new_cols_date = ["day_of_week", "month_of_year", "day_of_month", "time"]
+
+    tweets[new_cols_date] = tweets['date'].str.split(' ', expand=True)[[0,1,2,3]]
+    tweets["hour_of_day"] = tweets['time'].str.split(':', expand=True)[0].astype(int)
+
+    # Drop columns that are no important 
+    cols_to_drop = ["date", "time", "flag"]
+    tweets.drop(columns= cols_to_drop, inplace=True)
+
+    # Drop duplicates
+    tweets.drop_duplicates(subset= ['text','sentiment'], keep = 'first', inplace=True)
+    tweets.drop_duplicates(subset=['text'],inplace=True)
+    
     tweets["night"] = (tweets["hour_of_day"].astype("int") >= 18) | (tweets["hour_of_day"].astype("int") <= 5) # to verify with an histogram optimal times
     return tweets
 
-def text_mining(tweets): #must work on this function to improve perfomance
+def text_mining(tweets: pd.DataFrame) -> pd.DataFrame: #must work on this function to improve perfomance
     lemmaTokenizer = LemmaTokenizer()                                                                      
     vectorizer = TfidfVectorizer(tokenizer=lemmaTokenizer, stop_words=sw.words('english'), strip_accents="ascii", use_idf=False, min_df=0.01)
     tfidf = vectorizer.fit_transform(tweets["text"])
@@ -41,7 +56,18 @@ def text_mining(tweets): #must work on this function to improve perfomance
     tweets.drop(columns=["user", "text"], inplace=True) #For the moment
     return tweets
 
-def preprocessing1(tweets):
+def text_mining_sentiment(tweets: pd.DataFrame) -> pd.DataFrame:
+    sentiment = np.array(list(map(lambda x: list(SentimentIntensityAnalyzer().polarity_scores(x).values()), tqdm(tweets["text"]))))
+    tweets["neg"] = sentiment[:, 0]
+    tweets["neu"] = sentiment[:, 1]
+    tweets["pos"] = sentiment[:, 2]
+    tweets["compound"] = sentiment[:, 3]
+    tweets["polarity"] = list(map(lambda x: TextBlob(x).sentiment.polarity, tqdm(tweets["text"])))
+    tweets["subjectivity"] = list(map(lambda x: TextBlob(x).sentiment.subjectivity, tqdm(tweets["text"])))
+    tweets.drop(columns=["user", "text"], inplace=True) #For the moment
+    return tweets
+
+def preprocessing(tweets: pd.DataFrame) -> pd.DataFrame:
     day_of_week_dict = {"Mon": 1, "Tue": 2, "Wed": 3, "Thu": 4, "Fri": 5, "Sat": 6, "Sun": 7}
     months_dict = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}
     
@@ -51,10 +77,9 @@ def preprocessing1(tweets):
     tweets["hour_of_day"] = list(map(lambda x: int(x), tweets["hour_of_day"]))
     tweets["ids"] = ColumnTransformer([('somename', MinMaxScaler(), [1])], remainder='passthrough').fit_transform(tweets)
 
-    #y = tweets.pop("sentiment")
-    #X = tweets
-
     return tweets
+
+    
 
 
 def create_hours_bins(dataframe: pd.DataFrame) -> pd.DataFrame:
@@ -73,9 +98,3 @@ def create_pubblic_holiday(dataframe: pd.DataFrame) -> pd.DataFrame:
     dataframe["pubblic_holyday"] = list(map(lambda x: holydays_dict[x], dataframe["day_of_week"]))
     
     return dataframe
-
-
-
-def drop_dupl(dataframe: pd.DataFrame):
-    dataframe.drop_duplicates(subset= ['text','sentiment'], keep = 'first', inplace=True)
-    dataframe.drop_duplicates(subset=['text'],inplace=True )
