33437748e8ddedb2f42c9c7205968f192287d17f
Christian Montecchiani <69299238+ChristianMontecchiani@users.noreply.github.com>
2022-01-13 21:43:23 +0100

    Add files via upload
    Update di tuning.py and model_selection.py

--- model_selection.py
+++ model_selection.py
@@ -1,141 +1,69 @@
 import pandas as pd
-import numpy as np
-import seaborn as sns
 
-import time
-
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
-from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier
 from sklearn.naive_bayes import BernoulliNB
-
-from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+from sklearn.svm import LinearSVC
 from sklearn.model_selection import cross_val_score
 
-
-# Plot
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-
-# Feature Extraction
-import regex as re
-from nltk.tokenize import word_tokenize
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.corpus import stopwords as sw
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from textblob import TextBlob
-
-# See time 
-from tqdm import tqdm
 import time
 
-# PreProcessing
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.compose import ColumnTransformer
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
-# Model Selection
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
-
-# Parameters
-from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import train_test_split
-
-clf_models = [LinearSVC(), RandomForestClassifier(), BernoulliNB(), MLPClassifier()]
-clf_names = [' Linear Support Vector Machine', 'Random Forest', 'Nayve Bayes Bernoulli', 'Multi Layer Neural Network']
-
-
-def compare_models(X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.Series, y_test: pd.Series, models: list, names: list) -> pd.DataFrame:
-
-    """Inputs:
-        X_train = training data set
-        X_test = testing data set
-        y_train = training label 
-        y_test = testing label 
-        models = list of models to compare
-        names = list of trings containing the names of the models 
-
-        Return: comparison table 
-        """
-
-    f1_scores, precision_scores, recall_scores, accuracy_scores = [], [], [],[]
+def test_models(X : pd.DataFrame, y : pd.Series) -> pd.DataFrame:
+    # Test 4 models and print results 
+    models = [LinearSVC(random_state=42), RandomForestClassifier(random_state=42), BernoulliNB(), HistGradientBoostingClassifier(random_state=42)]
+    model_names= ["Linear SVC", "Random Forest", "Bernoulli Nayve-Bayes", "Hist GBC"]
     
-    for clf, name in zip(models, names):
-
-        print(f'Start training model: {name}')
-
-        start_time = time.time()
-
-        clf.fit(X_train, y_train)
-        print('\n')
-
-        finish_time = time.time()
-
-        print(f'Fiishing training model: {name}, trained in {finish_time-start_time}\n')
-
-        y_pred = clf.predict(X_test)
-
-        f1 = f1_score(y_test, y_pred, average = 'macro')
-        f1_scores.append(f1)
-
-    
-        print(f'Score of {name} model performed: {f1}')
-
-    col1 = pd.Series(names)
-    col2 = pd.Series(f1_scores)
-        
-
-    result = pd.concat([col1, col2], axis = 'columns')
-    result.columns = ['Model Name', 'F1 Score']
-    return result
-
-
-
-def compare_models_KF(X: pd.DataFrame, y: pd.Series, models: list, names: list, k = int) -> pd.DataFrame:
-
-    """Inputs:
-        X = training data set
-        y = training label  
-        models = list of models to compare
-        names = list of trings containing the names of the models 
-        k = int for the cross validation 
-
-    
-        Return: comparison table """
-
     f1_scores = []
-        
-    for clf, name in zip(models, names):
+
+    for clf, name in zip(models, model_names):
 
         print(f'Start training model: {name}')
 
         start_time = time.time()
-        f1_score = cross_val_score(clf, X, y, cv = k, scoring = 'f1_macro')
+        f1_score = cross_val_score(clf, X, y, cv = 5, scoring = 'f1_macro')
         finish_time = time.time()
 
-        print(f'Finishing training model: {name}, trained in {finish_time-start_time}\n')
+        print(f'Finishing training model: {name}, trained in {finish_time-start_time}')
 
         f1 = f1_score.mean()
         f1_scores.append(f1)
 
         print(f'Score of {name} model performed: {f1}\n\n\n')
 
-    col1 = pd.Series(names)
-    col2 = pd.Series(f1_scores)
-        
+        col1 = pd.Series(model_names)
+        col2 = pd.Series(f1_scores)
+            
 
     result = pd.concat([col1, col2], axis = 'columns')
     result.columns = ['Model Name', 'F1 Score']
-
+    
     return result
+
+
+def test_diff_preprocessing(X_train: pd.DataFrame, y_train: pd.DataFrame):
+    # Test all models with different preprocessing techiniques
+
+    # WE + TF-DF + Sentiment 
+    print("Test Word Embeddings + TF-DF + Sentiment: \n\n")
+    
+    result_1 = test_models(X_train, y_train)
+
+    
+    # TF-DF + Sentiment 
+    X_train.drop(columns=["embedding_negativity", "embedding_positivity"] , inplace=True)
+    print("Test TF-DF + Sentiment: \n\n")
+   
+    result_2 = test_models(X_train, y_train)
+
+    
+     # Only Sentiment 
+    cols_not_to_drop= ["ids", "day_of_week", "month_of_year", "day_of_month", "hour_of_day", "char_count", "neg", "neu", "pos", "compound", "polarity",
+    "subjectivity"]
+    col_tf_df =[col for col in X_train.columns if col not in cols_not_to_drop]
+    X_train.drop(columns=col_tf_df , inplace=True)
+    print("Test  Sentiment: \n\n")
+
+    result_3 = test_models(X_train, y_train)
+
+
+    return result_1, result_2, result_3
--- tuning.py
+++ tuning.py
@@ -1,62 +1,34 @@
 import numpy as np
-import pandas as pd
+
+from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import HalvingGridSearchCV
+from sklearn.model_selection import GridSearchCV
 
 
-from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier
+PARAMETERS_HGBC ={
+    "learning_rate":[0.1, 0.2, 0.3],
+    "max_iter": [150, 200],
+    "max_leaf_nodes": [20,30,40,None],
+    "min_samples_leaf":[ 2, 4, 10],
+    "l2_regularization" : [0, 0.1, 0.2, 0.3],
+    "early_stopping": [True, False]
+}
 
 
-
-bool_par = [True, False]
-
-
-# RANDOM FOREST
-
-parameters_random_forest = {
-    "n_estimators" : np.arange(start=500, stop = 5000, step = 500), 
+PARAMETERS_RF= {
     "max_features" : ["sqrt", "log2"], 
-    "max_depth ": np.arange(start= 10, stop = 30, step = 5), 
     "criterion": ["gini", "entropy"],
-    "n_jobs": -1, 
-    "min_samples_split" :[2, 5, 10],
-    "min_samples_leaf":[1, 2, 4],
-    "bootstrap" : bool_par
-}
-
-# SUPPORT VECTOR MACHINE 
-
-parameters_support_vector ={
-    "kernel": ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed' ],
-    "degree": np.arange(start=1, stop= 10, step = 1 ),
-    "verbose": bool_par,
-    "decision_function_shape": ['ovo', 'ovr'], 
-    "break_ties": bool_par,
-    "probability": bool_par, 
-    "shrinking": bool_par,
-    "gamma":['scale', 'auto'] 
-    }
-
-
-
-
-parameters_HGBC ={
-    "loss": ["auto", "binary_crossentropy", "categorical_crossentropy"],
-    "learning_rate": np.arange(start=0.1, stop=1, step=0.1),
-    "max_iter": np.arange(start=100, stop= 1000, step= 100),
-    "max_leaf_nodes": [20,25,30,35,50,None],
-    "min_samples_leaf":[10, 20, 40],
-    "l2_regularization" : np.arange(start=0, stop=1, step=0.1),
-    "warm_start": [True, False]
-
+    "min_samples_leaf":np.arange(10,70,10),
+    "min_samples_split": np.arange(2, 10, 1)  
 }
 
 
-def test_classifier(clf, X_train : pd.DataFrame, X_test : pd.DataFrame, y_train: pd.Series, file_name: str) -> None:
+def tuning_classifiers(clf, parameters_grid, X_train, y_train, k_fold = 3, normal_grid_search = False) -> None:
+    # Tune classifiers with Halving Search CV if "normal_grid_search" is false otherwise use Grid Search CV
+    if normal_grid_search: 
+        cv = GridSearchCV(clf, parameters_grid, cv=k_fold, scoring="f1_macro", n_jobs=-1).fit(X_train,y_train)
+    else:
+        cv =  HalvingGridSearchCV(clf, parameters_grid, cv=k_fold, scoring="f1_macro", n_jobs=-1, random_state = 42).fit(X_train,y_train)
 
-    print(f'Start Training with {clf}')
-    clf.fit(X_train,y_train)
-    print(f'Finish Training with {clf}')
-
-    y_pred = clf.predict(X_test)
-
-    pd.Series(y_pred, name="Predicted").to_csv(file_name, index_label="Id")
-    print(f"File Salvato con questo nome: {file_name}!\n\n\n")
+    print(cv.best_estimator_)
+    print(cv.best_score_)
